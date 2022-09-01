878ba03722e2b2cf754086d0bb6744b19330cb8d
faith okpala <okpalafaith1@gmail.com>
2022-09-01 16:50:03 +0100

    review

--- src/app.ts
+++ src/app.ts
@@ -14,9 +14,7 @@ import orderRouter from "./routes/orderRoutes";
 import reviewRouter from "./routes/reviewRoutes";
 const app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "..", "views"));
-app.set("view engine", "ejs");
+
 
 app.use(logger("dev"));
 app.use(express.json());
